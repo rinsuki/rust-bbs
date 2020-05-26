3d0d4682b08c065a187f5064b7018570e17d6483
rinsuki <428rinsuki+git@gmail.com>
2020-05-27 06:46:50 +0900

    Hello, world! with tide

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ extern crate async_std;
 extern crate tide;
 
 #[async_std::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<(), std::io::Error> {
+    let mut app = tide::new();
+    app.at("/").get(|_| async { Ok("Hello, world!") });
+    app.listen("0.0.0.0:3000").await
 }

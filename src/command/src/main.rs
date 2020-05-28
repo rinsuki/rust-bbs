86afa49e4e6f41700be248620567ea2c5700feb9
rinsuki <428rinsuki+git@gmail.com>
2020-05-28 18:50:31 +0900

    listen する前にメッセージ出すように

--- src/command/src/main.rs
+++ src/command/src/main.rs
@@ -4,5 +4,6 @@ extern crate async_std;
 #[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
     let app = app::create();
+    println!("Let's start!!");
     app.listen("0.0.0.0:3000").await
 }

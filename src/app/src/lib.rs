151caa03fe5e534fff4002b1361fe74de95de41b
rinsuki <428rinsuki+git@gmail.com>
2020-05-29 09:24:57 +0900

    板トップの表示

--- src/app/src/lib.rs
+++ src/app/src/lib.rs
@@ -3,7 +3,7 @@ extern crate horrorshow;
 extern crate mime;
 extern crate tide;
 
-use diesel::prelude::RunQueryDsl;
+use diesel::prelude::*;
 mod views;
 
 pub fn create() -> tide::Server<()> {
@@ -17,6 +17,17 @@ pub fn create() -> tide::Server<()> {
         let resp = views::top(&boards)?;
         return Ok(tide::Response::new(tide::StatusCode::Ok)
             .body_string(resp)
+            .set_mime(mime::TEXT_HTML));
+    });
+    app.at("/:board/").get(|req: tide::Request<()>| async move {
+        let db = database::connection::establish();
+        let board: String = req.param("board")?;
+        let board: database::models::Board = {
+            use database::schema::boards::dsl::boards;
+            boards.find(board).first(&db)
+        }?;
+        return Ok(tide::Response::new(tide::StatusCode::Ok)
+            .body_string(views::board_top(&board)?)
             .set_mime(mime::TEXT_HTML));
     });
     return app;
--- src/app/src/views.rs
+++ src/app/src/views.rs
@@ -25,3 +25,26 @@ pub fn top(boards: &std::vec::Vec<database::models::Board>) -> Result<String, ho
     }
     .into_string();
 }
+
+pub fn board_top(board: &database::models::Board) -> Result<String, horrorshow::Error> {
+    return html! {
+        : doctype::HTML;
+        html {
+            head {
+                meta(charset="UTF-8");
+                title : format!("{} - rust-bbs", board.name);
+            }
+            body {
+                table(width="95%", cellspacing=7, cellpadding=3, border=1, bgcolor="#CFC", align="center") {
+                    tr {
+                        td(style="padding: 0.5rem") {
+                            h1(style="margin: 0; margin-bottom: 1rem") : board.name.to_string();
+                            center : "ここに説明文を書く";
+                        }
+                    }
+                }
+            }
+        }
+    }
+    .into_string();
+}
